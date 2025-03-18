8be7b034efc052565327e2c116fac99d0ffb3dd7
mbrilla <mjblebrilla@gmail.com>
2025-03-18 18:03:32 +0800

    GH-Pages Implementation

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,13 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/fashion-advisor/',
   server: {
     port: 3000,
     host: true
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: true
   }
 })
